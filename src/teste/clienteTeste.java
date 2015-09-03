90b4cce844afc91014977694c26081aeb35fd00e
CristianoPegoraro <cristianopegoraro@msn.com>
2015-09-03 19:45:50 -0300

    Cliente

--- src/teste/clienteTeste.java
+++ src/teste/clienteTeste.java
@@ -11,9 +11,14 @@ public class clienteTeste {
 	public static void main(String[]args){
 		ClienteDAO clienteDAO = new ClienteDAOJDBC();
 		Cliente cliente = new Cliente();
+		cliente.setCodigo(3);
 		cliente.setNome("Diovana");
+		cliente.setCpf("11111111111");
+		cliente.setRg("6094465");
+		cliente.setEndereco("Rua joao lunardi, 146n, bairro alvorada");
+		cliente.setTelefone("33548956");
+		
 		clienteDAO.inserir(cliente);	
-		
-		
+				
 	}
 }
